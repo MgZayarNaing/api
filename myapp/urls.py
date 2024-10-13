3cd5b0f0a68fac3845e4824b7924ccfe0308ceda
MgZayarNaing <zayarnaing.st@gmail.com>
2024-10-13 13:24:29 +0630

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import categories_views
 
 urlpatterns = [
+    path('categories/', categories_views.categories_list, name='categories-list'),
+    path('categories/create/', categories_views.categories_create, name='categories_create'),
 
 ]
